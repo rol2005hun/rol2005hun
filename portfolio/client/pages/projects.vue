04a76ff6a51558789190a7ba3a3e88077dcee160
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-08-07 16:12:47 +0300

    Add files via upload

--- portfolio/client/pages/projects.vue
+++ portfolio/client/pages/projects.vue
@@ -2,7 +2,10 @@
     <main class="projects">
         <h1><i class="fa-solid fa-diagram-project"></i> Projektek</h1>
         <br>
+        <h3><a href="https://account.wolimby.hu" class="link">Wolimby Account (Kötelező bejelentkezés)</a></h3>
         <h3><a href="https://games.wolimby.hu" class="link">Wolimby Games</a></h3>
+        <h3><a href="https://shortify.wolimby.hu" class="link">Wolimby Shortify</a></h3>
+        <h3><a href="https://social.wolimby.hu" class="link">Wolimby Social (Kötelező bejelentkezés)</a></h3>
         <h3><a href="https://github.com/rol2005hun/cpp" class="link">C++ repository GitHubon</a></h3>
     </main>
 </template>
