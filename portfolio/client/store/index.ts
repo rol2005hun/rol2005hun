2ad792650d721fc98d4ff680ac24621cb08493da
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-11-06 13:54:35 +0200

    Delete portfolio/client directory

--- portfolio/client/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { createPinia } from 'pinia';
-import { useNotificationStore } from '@/store/modules/notification';
-
-export const pinia = createPinia();
-
-export const notificationStore = useNotificationStore(pinia);
