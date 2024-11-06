1e9828f9f79941dccd065b4ab227fb8b0c9f5bf2
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-11-06 12:55:38 +0100

    Add files via upload

---
+++ client/components/footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+        <h2>Készítette rol2005hun ❤️-el és 💖-al - <a href="https://github.com/rol2005hun/rol2005hun/tree/main/portfolio/client" target="_blank">Forráskód</a></h2>
+    </footer>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@use '@/assets/scss/footer.scss';
+</style>
