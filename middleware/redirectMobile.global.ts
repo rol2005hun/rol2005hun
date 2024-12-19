27f14ee2921ae5d59039d28dee24c72943fe621c
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-12-19 21:43:37 +0200

    Delete middleware directory

--- middleware/redirectMobile.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const userAgent = useRequestHeaders()['user-agent'] || '';
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-
-    if (isMobile) {
-        return navigateTo('https://ranzak.site', { external: true });
-    }
-});
